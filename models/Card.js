2a2541b36e18ffc4eb0bf1c20815a2107853b3fd
Jack Howard <Jhoward@realnets.com>
2018-05-14 15:00:08 -0500

    Abastract out model of card

---
+++ models/Card.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+// Set up default mongoose connection
+const mongoDB = 'mongodb://localhost/my_database';
+
+mongoose.connect(mongoDB);
+
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+
+// Get the default connection
+var db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+let Schema = mongoose.Schema;
+
+// define properties casted to SchemaTypes
+// may want to use ObjectId instead 
+// this is the model schema
+let cardSchema = new Schema({
+	trelloCardId: String,
+	cwCardId: String,
+	status: String
+});
+
+// convert schema definition into a Model that's accessible 
+let Card = mongoose.model('Card', cardSchema);
+
+module.exports = Card;
