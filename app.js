2fab9cb185e1759218e7a551b95c487cc92d161d
Jack Howard <Jhoward@realnets.com>
2018-05-14 13:48:54 -0500

    Refactor dependencies, add webhook parser and creator, count matches

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+"use strict";
+
 // key values hidden using .env file -- do not commit
 // via https://github.com/motdotla/dotenv
 require('dotenv').config();
 
-// secret auth values
-const trelloKey = process.env.TRELLO_API_KEY;
-const trelloToken = process.env.TRELLO_API_TOKEN;
-const connectWiseApiKey = process.env.CW_API_KEY;
+let express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	trelloKey = process.env.TRELLO_API_KEY,
+	trelloToken = process.env.TRELLO_API_TOKEN,
+	connectWiseApiKey = process.env.CW_API_KEY,
+	axios = require('axios'),
+	mongoose = require('mongoose');
 
-// http client
-const axios = require('axios');
+// read the payload from the webhook
+app.use(bodyParser.json());
 
 // trello info 
 const inProgressTrelloListId = '5af5c45c81c07bd0e39b2ad4';
@@ -19,11 +25,9 @@ const trelloServiceBoard = '5af5a9c2c93fd3f22b4c71fe';
 
 // cw info 
 // longer time frame filter
-// const cwServiceBoard = 'https://realnets+' + connectWiseApiKey = '@api-na.myconnectwise.net/v4_6_release/apis/3.0/service/tickets?conditions=board/name="Dev Tickets" AND lastUpdated > [2018-04-20T00:00:00Z]';
-const cwServiceBoard = 'https://realnets+' + connectWiseApiKey + '@api-na.myconnectwise.net/v4_6_release/apis/3.0/service/tickets?conditions=board/name="Dev Tickets" AND lastUpdated > [2018-05-10T00:00:00Z]';
+const olderCwServiceBoard = 'https://realnets+' + connectWiseApiKey + '@api-na.myconnectwise.net/v4_6_release/apis/3.0/service/tickets?conditions=board/name="Dev Tickets" AND lastUpdated > [2018-04-20T00:00:00Z]';
 
-// mongo db stuff 
-const mongoose = require('mongoose');
+const cwServiceBoard = 'https://realnets+' + connectWiseApiKey + '@api-na.myconnectwise.net/v4_6_release/apis/3.0/service/tickets?conditions=board/name="Dev Tickets" AND lastUpdated > [2018-05-10T00:00:00Z]';
 
 //Set up default mongoose connection
 const mongoDB = 'mongodb://localhost/my_database';
@@ -47,42 +51,38 @@ let Schema = mongoose.Schema;
 let cardSchema = new Schema({
 	trelloCardId: String,
 	cwCardId: String,
-	status: String 
+	status: String
 });
 
 // // convert schema definition into a Model that's accessible 
 let Card = mongoose.model('CardList', cardSchema);
 
-parseCWBoard();
+// createWebhook();
+// async function createWebhook() {
+// 	try {
+// 		// use board id to listen
+// 		// http://53f47b43.ngrok.io
+// 		const boardWebhook = await axios.post("https://api.trello.com/1/webhooks/", {
+// 			description: 'Listen for board changes',
+// 			callbackURL: 'https://c70f1dd7.ngrok.io/board-change',
+// 			idModel: trelloServiceBoard,
+// 			key: trelloKey,
+// 			token: trelloToken,
+// 			active: true
+// 		});
+// 		console.log("Board web hook " + boardWebhook);
+// 	} catch (err) {
+// 		console.log('Something went wrong when creating webhook');
+// 		console.error(err);
+// 	}
+// }
 
-// check db
-// findALlDb();
+// clearDb();
+run();
 
-// axios is nice because it returns json just as you'd expect 
-function parseCWBoard() {
-	axios.get(cwServiceBoard).then(results => {
-		// look in return value of data 
-		let tickets = results.data;
-
-		// suppose I could map this with only values I need
-		// last value wasn't reading huh with <=
-		// don't need to return anything here just loop thru
-		// therefore for loop ok
-		for (let i = 0; i < tickets.length; i++) {
-			// need to check that an id for cw and trello doesn't exist already 
-			// if it does exist already, should I update it if the status has changed? 
-			let ticket = tickets[i];
-
-			createTrelloCard(ticket.id, ticket.status.name, ticket.summary);
-		}
-	}).catch(error => console.log(error));
-}
-
-// make a new card in trello upon startup
-function createTrelloCard(cwCardId, status, summary) {
-	// needs to go in to axios as a string not Number 
-	let stringCardId = cwCardId.toString();
-
+async function run() {
+	const tickets = await parseCWBoard();
+	// console.log(tickets);
 	// if cw id does not exist in the db  
 	//    if the status for that cw id has changed 
 	//       then move the card in trello and update status in the db 
@@ -91,75 +91,181 @@ function createTrelloCard(cwCardId, status, summary) {
 	// else 
 	//    then create that new trello card from cw 
 
+	for (let i = 0; i < tickets.length; i++) {
+		const [ticketExists] = await Promise.all([cwTicketAlreadyExists(tickets[i].id)]);
+
+		// just use a default here 
+		// moveTrelloCard();
+
+		// if cw id does exist in the db  
+		if (ticketExists === true) {
+			// don't make a new ticket
+			console.log('ticket already exists');
+
+			// will be renaming these params on the other side to directly match in db
+			const [statusChanged] = await Promise.all([cwTicketStatusChanged(tickets[i].id, tickets[i].status.name)]);
+			// if the status for that cw id has changed 
+			if (statusChanged === true) {
+				// then move the card in trello 
+				// update status in the db 
+
+				// need to find the db for matching trello id and db's status 
+				// need to hardcode the change of a cw board ticket 
+
+				// hardcoding moving the change back to open 
+				// moveTrelloCard(trelloCardId, targetTrelloListId);
+			}
+		} else {
+			// then create that new trello card from cw 
+			const newTrelloCard = await createNewTrelloCard(tickets[i].id, tickets[i].status.name, tickets[i].summary);
+
+			// save to db 
+			createCard(newTrelloCard.id, tickets[i].id, tickets[i].status.name);
+			console.log("done making one card");
+		}
+	}
+	console.log("done making tickets");
+}
+
+
+async function parseCWBoard() {
+	const cwPromise = axios.get(olderCwServiceBoard);
+	const [cwBoard] = await Promise.all([cwPromise]);
+
+	return cwBoard.data;
+}
+
+async function cwTicketAlreadyExists(cwCardId) {
+	try {
+		// need to find if there's more than 1 match 
+		// if there's more than one match that's a problem in the future
+		const cardMatchCount = await Card.count({
+			cwCardId
+		}).exec();
+		console.log("amount of matches found: " + cardMatchCount);
+		return cardMatchCount > 0;
+	} catch (err) {
+		console.log('Something went wrong when trying to find the matching cw card id')
+	}
+}
+
+async function cwTicketStatusChanged(cwCardId, status) {
+	try {
+		// need to find if there's more than 1 match 
+		// if there's more than one match that's a problem in the future
+		// using es6 object matching {cwCardId: cwCardId, status: status} is equivalent
+		const cardMatchCount = await Card.count({
+			cwCardId,
+			status
+		}).exec();
+		console.log("amount of matches found: " + cardMatchCount);
+
+		// we can safely assume that cwCardId is a match already 
+		// status has changed 
+		// so if status has changed 
+		// it will be no match zero 
+		return cardMatchCount === 0;
+	} catch (err) {
+		console.log('Something went wrong when trying to find the matching cw card id and its status')
+	}
+}
+
+async function createNewTrelloCard(cwCardId, status, summary) {
+	// needs to be string for post
+	let stringCardId = cwCardId.toString();
+
 	// determine which list the card will go in 
 	// will need to make a better dictionary key check with matching
 	let boardId = status === 'Triage' ? openTrelloListId : inProgressTrelloListId;
 
-	axios.post('https://api.trello.com/1/cards', {
+	let trelloCardPromise = axios.post('https://api.trello.com/1/cards', {
 		name: summary,
 		idList: boardId,
 		keepFromSource: 'all',
 		key: trelloKey,
 		token: trelloToken
-	})
-	.then(response => {
-		// this is the corresponding trello id card
-		// will need to update the db entry based on this new field 
-		createCard(response.data.id, cwCardId, status);
-	})
-		.catch(error => {
-		console.log(error);
 	});
+
+	const [trelloCard] = await Promise.all([trelloCardPromise]);
+	return trelloCard.data;
 }
 
 // make the instance for mongo db based on the model
 function createCard(trelloCardId, cwCardId, status) {
-	let stringTrelloCardId = trelloCardId.toString(), 
-	stringCwCardId = cwCardId.toString();
+	let stringTrelloCardId = trelloCardId.toString(),
+		stringCwCardId = cwCardId.toString();
 
 	let card = new Card({
 		cwCardId: stringCwCardId,
 		trelloCardId: stringTrelloCardId,
 		status: status
 	});
-	card.save(function(err, card) {
+	card.save(function (err, card) {
 		if (err) return console.error(err);
 		console.log(card);
 	});
 }
 
-// // test to make sure things are saving properly
+// update trello card list based on change
+async function moveTrelloCard(trelloCardId = '5af9bdb92aa3910bd7a42d38', targetTrelloListId = openTrelloListId) {
+	try {
+		let trelloCardPromise = axios.put('https://api.trello.com/1/cards/' + trelloCardId, {
+			idList: targetTrelloListId,
+			key: trelloKey,
+			token: trelloToken
+		});
+		const [trelloCard] = await Promise.all([trelloCardPromise]);
+		console.log(trelloCard.data);
+	} catch (err) {
+		console.log(err)
+	}
+}
 
+
+process.on('unhandledRejection', error => {
+	console.log('unhandledRejection', error);
+});
 
 // quick sanity check
 function findALlDb() {
 	console.log("checking all entries");
-	Card.find({}, 'status', (err, cards) => {
-		if(err){
+	Card.find({}, (err, cards) => {
+		if (err) {
 			console.log(err);
-		} else{
+		} else {
 			// check on the last one just to make sure
-			console.log('list of statuses', cards.length, cards[0].status);
+			console.log(cards.length);
 		}
 	})
 
-	// console.log("clearing all entries");
-	// Card.remove({}, (err, cards) => {
-	// 	if (err) {
-	// 		console.log(err)
-	// 	} else {
-	// 		console.log("deleting all");
-	// 	}
-	// })
+
 }
 
-// will need to utilize express later for listening for web hooks or starting
-// const express = require('express');
-// const app = express();
+function clearDb() {
+	console.log("clearing all entries");
+	Card.remove({}, (err, cards) => {
+		if (err) {
+			console.log(err)
+		} else {
+			console.log("deleting all");
+		}
+	})
+}
 
 // express ability to listen for webhook changes stub
-// // hello world 
-// app.get('/', (req, res) => res.send('Hello World!'));
+// listening on port 3000 
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
-// // listening on port 3000 
-// app.listen(3000, () => console.log('Example app listening on port 3000!'));
+// web hooks for listening for board change
+// this is where ngrok is being forwarded to
+app.post('/board-change', (req, res) => {
+	res.status(200).send('board change');
+	console.log('board change post');
+	console.log(req.body.action);
+
+});
+
+app.get('/board-change', (req, res) => {
+	res.status(200).send('board change');
+	console.log("board get");
+});
