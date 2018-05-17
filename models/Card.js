cfa3fc64c5f7c23f03e64053f6db0d212a8bb44d
Jack Howard <Jhoward@realnets.com>
2018-05-17 10:37:27 -0500

    Style the strings based on airbnb style

--- models/Card.js
+++ models/Card.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const mongoDB = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0-8kzdx.mongodb.net/test?retryWrites=false`;
 // Set up default mongoose connection
-// const mongoDB = 'mongodb://localhost/my_database';
+// const mongoDB = "mongodb://localhost/my_database";
 
 mongoose.connect(mongoDB);
 
@@ -10,25 +10,25 @@ mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 
 // Get the default connection
-var db = mongoose.connection;
+let db = mongoose.connection;
 
 // Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 let Schema = mongoose.Schema;
 
 // define properties casted to SchemaTypes
-// may want to use ObjectId instead 
+// may want to use ObjectId instead
 // this is the model schema
 let cardSchema = new Schema({
-	trelloCardId: String,
-	cwCardId: String,
-	status: String
+    trelloCardId: String,
+    cwCardId: String,
+    status: String
 });
 
-// convert schema definition into a Model that's accessible 
-let Card = mongoose.model('Card', cardSchema);
+// convert schema definition into a Model that"s accessible
+let Card = mongoose.model("Card", cardSchema);
 
-// mongoose.set('debug', true);
+// mongoose.set("debug", true);
 
 module.exports = Card;
